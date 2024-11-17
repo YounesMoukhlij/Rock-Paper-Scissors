8b55f630e02d42341c2b381a05473950e526c03d
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-17 15:19:37 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -12,5 +12,5 @@ function getRandomComputerPlay ()
 
 function isPlayerWon(computer, player)
 {
-	return (player == "Rock" && computer == "Paper" )
+	return (player == "Rock" && computer == "Paper" || player == )
 }
