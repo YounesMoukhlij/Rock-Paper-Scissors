eebffce037d4ddf498d808a4aa4e0b890c770550
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-17 15:16:21 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -4,5 +4,6 @@ const arrayPlays = ["Rock", "Paper", "Scissors"];
 
 function getRandomComputerPlay ()
 {
-	return (MATH.floor(Math.floor() 8 arrayPlays.length));
+	return (MATH.floor(Math.floor() * arrayPlays.length));
 }
+
