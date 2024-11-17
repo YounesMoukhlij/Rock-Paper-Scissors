dfa31167f69999c3fa771b8971dd4e8ca70b090f
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-17 15:21:36 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -14,5 +14,5 @@ function isPlayerWon(computer, player)
 {
 	return (player == "Rock" && computer == "Scissors" ||
 			player == "Scissors" && computer == "Paper" ||
-			player == "Paper" )
+			player == "Paper" && computer == "")
 } 
