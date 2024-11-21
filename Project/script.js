43581220c3a3966aeb401619cc7da47ae300ffdf
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-21 16:46:03 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -47,7 +47,7 @@ let rock_button = document.getElementById("Rock");
 let scissors_button = document.getElementById("Scissors");
 let paper_button = document.getElementById("Paper");
 
-let	optionContainer = document.getElementById(".button-")
+let	optionContainer = document.getElementById(".button-container");
 
 
 function resetGame()
