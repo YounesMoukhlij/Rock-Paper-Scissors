e7d7c39fa5b036311e2ff1b3115f782fbeadbaac
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-21 15:44:51 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -21,5 +21,5 @@ function getResult(userOption)
 {
 	let computerChoice = getRandomComputerChoice();
 
-	if (isPlayerWons(userOption, computerChoice)
+	if (isPlayerWons(userOption, computerChoice))
 }
