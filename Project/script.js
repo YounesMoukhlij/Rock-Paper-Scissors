64d2603e710a734cafbafb5e90b0a52492f7da10
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-21 16:36:24 +0100

    by gacp

--- Project/script.js
+++ Project/script.js
@@ -68,7 +68,7 @@ function Game(userOption)
 
 	if (PlayerScore == 3 || ComputerScore == 3) {
 			
-		winner.innerText = ` "AI Gonna Eat You Someday!";
+		winner.innerText = `${PlayerScore } "AI Gonna Eat You Someday!";
 		resetGame();
 	}
 	i++;
